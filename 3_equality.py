1eacb8f5745ff18793ea28ac618e80bde4dae678
yqu212 <yqu212@gmail.com>
2014-03-05 13:32:26 +0800

    Add an answer to the third level.

---
+++ 3_equality.py
@@ -0,0 +1,10 @@
+import re
+
+txt = open("d:\desktop\data.txt")
+pattern = re.compile(r"[a-z][A-Z]{3}[a-z][A-Z]{3}[a-z]")
+s = ""
+for line in txt:
+    guard = pattern.findall(line)
+    for string in guard:
+        s += string[4]
+print(s)
